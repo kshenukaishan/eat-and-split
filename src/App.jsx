3c7204a74b52bf64e615f5527daecb08d7367a88
kshenukaishan <kshenukaishan@outlook.com>
2023-10-19 23:24:10 +0530

    built static list of friends

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,42 @@ const initialFriends = [
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+      </div>
     </div>
+  );
+}
+
+function FriendList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          You have {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <button className="button">Select</button>
+    </li>
   );
 }
